e053a1d0389f49c230aed143749aae64cbd6a0ee
shenzhizhi <779822420@qq.com>
2017-02-17 00:57:39 +0800

    哈希
    哈希代码

---
+++ hash.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class hash {
+	private HashMap<Integer,String> coinname= new  HashMap<Integer,String>();
+	public static void main(String[] args) {
+	
+
+	}
+
+}
